ead879bd44fe26a24afab34209ab80e7561ee127
Ming <zhaomingming455@163.com>
2022-07-23 14:09:03 +0800

    fix

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,60 +1,61 @@
+import { defineComponent, ref } from '@vue/runtime-core'
 import Item from './Item'
-export default {
-  name: 'HelloWorld',
-  components: {Item},
-  data () {
-    return {
-      keys: [1, 2, 3, 4, 5],
-      values: {
-        1: 'first',
-        2: 'second',
-        3: 'third',
-        4: 'forth',
-        5: 'fifth'
-      },
-      data: [
-        {value: 'first'},
-        {value: 'second'},
-        {value: 'third'},
-        {value: 'forth'}
-      ],
-      title: '123'
-    }
-  },
-  methods: {
-    changeValue () {
-      this.values[1] = this.values[1] === 'X-change' ? 'first' : 'X-change'
-     },
-     handleChangeData () {
-       this.data.splice(0, 1, {value: this.data[0] === 'X-change' ? 'first' : 'X-change'})
-     },
-     handleChangeTitle () {
-        this.title = 'x-title'
-     }
-  },
-  render () {
-    const { keys, values, changeValue, data, handleChangeData, title, handleChangeTitle } = this
 
+export default defineComponent(() => {
+
+  const keys = ref([1, 2, 3, 4, 5])
+  const values = ref({
+    1: 'first',
+    2: 'second',
+    3: 'third',
+    4: 'forth',
+    5: 'fifth'
+  })
+  const data = ref([
+    {value: 'first'},
+    {value: 'second'},
+    {value: 'third'},
+    {value: 'forth'}
+  ])
+  const title = ref('123')
+
+  const handleChangeTitle = () => {
+    title.value = title.value === 'X-Change' ? '123' : 'X-Change'
+  }
+
+  const changeValue = () => {
+    values.value[1] = values.value[1] === 'X-change' ? 'first' : 'X-change'
+  }
+
+  const handleChangeData = () => {
+    console.log('data.value[0]', data.value[0])
+    data.value.splice(0, 1, {value: data.value[0].value === 'X-change' ? 'first' : 'X-change'})
+  }
+
+  const render = () => {
     return (
       <div>
         <button onClick={handleChangeTitle}>changeTitle</button>
-        <h1>{title}</h1>
-        <a style="cursor: pointer" onClick={changeValue}>change</a>
+        <h1>{title.value}</h1>
+        <button style="cursor: pointer" onClick={changeValue}>change</button>
         {
-          keys && keys.map(item => {
-            console.log('item::', item)
-            return (<Item key={item} dataKey={item} data={values}></Item>)
+          keys.value && keys.value.map(item => {
+            return (<Item key={item} dataKey={item} data={values.value}></Item>)
           })
         }
         <div style="padding-top: 20px">
           <button onClick={handleChangeData}>changeData</button>
           {
-            data && data.map(item => {
+            data.value && data.value.map(item => {
               console.log('dataItem', item)
               return (<Item key={item.value} dataKey='value' data={item}></Item>)
             })
           }
         </div>
-      </div>)
+      </div>
+    )
   }
-}
+
+  return render
+
+})
